62dea93062840263f22c169758be60a2c78ec18b
Filipi1503 <114451272+Filipi1503@users.noreply.github.com>
2025-07-13 20:51:09 -0300

    configurando ambiente

---
+++ src/main/java/com/sputnik/suputnik/SuputnikApplication.java
@@ -0,0 +1,13 @@
+package com.sputnik.suputnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuputnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuputnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sputnik/suputnik/SuputnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sputnik.suputnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuputnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
