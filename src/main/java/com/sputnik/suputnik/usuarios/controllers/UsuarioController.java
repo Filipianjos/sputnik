5d0f4bc924c596156bd01ad6da6f1bd42f3e98e9
Filipi1503 <114451272+Filipi1503@users.noreply.github.com>
2025-07-16 20:34:46 -0300

    criando testes unitarios cadastrar usuario

--- src/main/java/com/sputnik/suputnik/usuarios/controllers/UsuarioController.java
+++ src/main/java/com/sputnik/suputnik/usuarios/controllers/UsuarioController.java
@@ -1,4 +1,13 @@
 package com.sputnik.suputnik.usuarios.controllers;
 
+import com.sputnik.suputnik.usuarios.models.UsuarioModel;
+import com.sputnik.suputnik.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 public class UsuarioController {
+
 }
