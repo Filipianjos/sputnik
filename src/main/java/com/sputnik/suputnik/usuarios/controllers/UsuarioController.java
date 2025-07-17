a1b35d82d54860e5f9558182e2a294bbb57ba37e
Filipi1503 <114451272+Filipi1503@users.noreply.github.com>
2025-07-16 21:23:19 -0300

    adicicionado o metodo controller para  cadastrar usuario

--- src/main/java/com/sputnik/suputnik/usuarios/controllers/UsuarioController.java
+++ src/main/java/com/sputnik/suputnik/usuarios/controllers/UsuarioController.java
@@ -1,13 +1,21 @@
 package com.sputnik.suputnik.usuarios.controllers;
 
 import com.sputnik.suputnik.usuarios.models.UsuarioModel;
-import com.sputnik.suputnik.usuarios.repositories.UsuarioRepository;
+import com.sputnik.suputnik.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
 
+    @PostMapping
+    public UsuarioModel novoUsuario(@RequestBody UsuarioModel usuarioModel){
+        return usuarioService.criarUsuario(usuarioModel);
+    }
 }
